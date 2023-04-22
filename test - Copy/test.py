b7a98a407745da17e7f0080a539bcbd9091fa276
chu-andrew <chu.andrew.h@gmail.com>
2023-04-22 13:20:49 -0700

    newlines after form submit

--- test - Copy/test.py
+++ test - Copy/test.py
@@ -2,89 +2,29 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-from dataclasses import dataclass
-
-import requests
-
-
-@dataclass
-class YelpPlace:
-    name: str
-    rating: float
-    review_count: int
-    url: str
-
-
-def search_yelp(query="Restaurants", city="Los Angeles, CA"):
-    search_url = f"https://www.yelp.com/search/snippet?find_desc={query}&find_loc={city}&request_origin=user"
-    search_response = requests.get(search_url)
-    search_results = search_response.json()['searchPageProps']['mainContentComponentsListProps']
-
-    yelp_results = []
-    for result in search_results:
-        if result['searchResultLayoutType'] == "iaResult":
-            name = result['searchResultBusiness']['name']
-            rating = result['searchResultBusiness']['rating']
-            review_count = result['searchResultBusiness']['reviewCount']
-            url = "https://www.yelp.com" + result['searchResultBusiness']['businessUrl']
-
-            x = YelpPlace(
-                name, rating, review_count, url
-            )
-
-            yelp_results.append(x)
-
-    return yelp_results
+import yelp
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def my_form():
-    return render_template("my-form.html") # This should be the name of your HTML file
+    return render_template("my-form.html")  # This should be the name of your HTML file
+
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text1 = request.form['city']
-    results = search_yelp("ice cream", text1)
+    location = request.form['city']
+    results = yelp.search_yelp("ice cream", location)
     if len(results) == 0:
         return "No results found."
     else:
-        out=""
+        output = ""
         for location in results:
-            out = f'{out} {location.name} --- {location.rating} STARS ({location.review_count} reviews)\n {location.url}'
-        return out
+            line = f"<p>{location.name} --- {location.rating} STARS<p>"
+            output += line
+        return output
+
 
 if __name__ == '__main__':
     app.run()
-
-from dataclasses import dataclass
-
-import requests
-
-@dataclass
-class YelpPlace:
-    name: str
-    rating: float
-    review_count: int
-    url: str
-
-def search_yelp(query="Restaurants", city="Los Angeles, CA"):
-    search_url = f"https://www.yelp.com/search/snippet?find_desc={query}&find_loc={city}&request_origin=user"
-    search_response = requests.get(search_url)
-    search_results = search_response.json()['searchPageProps']['mainContentComponentsListProps']
-
-    yelp_results = []
-    for result in search_results:
-        if result['searchResultLayoutType'] == "iaResult":
-            name = result['searchResultBusiness']['name']
-            rating = result['searchResultBusiness']['rating']
-            review_count = result['searchResultBusiness']['reviewCount']
-            url = "https://www.yelp.com" + result['searchResultBusiness']['businessUrl']
-
-            x = YelpPlace(
-                name, rating, review_count, url
-            )
-
-            yelp_results.append(x)
-
-    return yelp_results
