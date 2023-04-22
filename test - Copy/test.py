8b942d98203dd44879aa9e0487ddf58e90f4ff96
Vincent Liu <vincebliu@ucla.edu>
2023-04-22 14:09:36 -0700

    few things

--- test - Copy/test.py
+++ test - Copy/test.py
@@ -6,6 +6,8 @@ from dataclasses import dataclass
 
 import requests
 
+import random
+
 
 @dataclass
 class YelpPlace:
@@ -45,46 +47,24 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text1 = request.form['city']
-    results = search_yelp("ice cream", text1)
+    results = search_yelp("breakfast", text1)
+    lunch_results = search_yelp("lunch", text1)
+    dinner_results = search_yelp("dinner", text1)
+    day_results = search_yelp(request.form['day'], text1)
+    afternoon_results = search_yelp(request.form['afternoon'], text1)
     if len(results) == 0:
         return "No results found."
     else:
-        out=""
-        for location in results:
-            out = f'{out} {location.name} --- {location.rating} STARS ({location.review_count} reviews)\n {location.url}'
-        return out
+        breakfast = results[random.randint(0, 9)]
+        lunch = lunch_results[random.randint(0, 9)]
+        dinner = dinner_results[random.randint(0, 9)]
+        day = day_results[random.randint(0, 9)]
+        afternoon = afternoon_results[random.randint(0, 9)]
+        return f'''You Will Eat Breakfast At: {breakfast.name} --- {breakfast.rating} STARS ({breakfast.review_count}) {breakfast.url}<br />
+            Then You Will Visit: {day.name} --- {day.rating} STARS ({day.review_count}) {day.url}<br />
+            You Will Eat Lunch At: {lunch.name} --- {lunch.rating} STARS ({lunch.review_count}) {lunch.url}<br />
+            Then You Will Visit: {afternoon.name} --- {afternoon.rating} STARS ({afternoon.review_count}) {afternoon.url}<br />
+            You Will Eat Dinner At: {dinner.name} --- {dinner.rating} STARS ({dinner.review_count}) {dinner.url}'''
 
 if __name__ == '__main__':
     app.run()
-
-from dataclasses import dataclass
-
-import requests
-
-@dataclass
-class YelpPlace:
-    name: str
-    rating: float
-    review_count: int
-    url: str
-
-def search_yelp(query="Restaurants", city="Los Angeles, CA"):
-    search_url = f"https://www.yelp.com/search/snippet?find_desc={query}&find_loc={city}&request_origin=user"
-    search_response = requests.get(search_url)
-    search_results = search_response.json()['searchPageProps']['mainContentComponentsListProps']
-
-    yelp_results = []
-    for result in search_results:
-        if result['searchResultLayoutType'] == "iaResult":
-            name = result['searchResultBusiness']['name']
-            rating = result['searchResultBusiness']['rating']
-            review_count = result['searchResultBusiness']['reviewCount']
-            url = "https://www.yelp.com" + result['searchResultBusiness']['businessUrl']
-
-            x = YelpPlace(
-                name, rating, review_count, url
-            )
-
-            yelp_results.append(x)
-
-    return yelp_results
